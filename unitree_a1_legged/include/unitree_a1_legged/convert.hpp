99128396677afa1c2ef2a7d3d04d1a205491cb48
Maciej Krupka <maciej.p.krupka@student.put.poznan.pl>
2023-11-03 21:43:39 +0100

    feat: add udp client
    Signed-off-by: Maciej Krupka <maciej.p.krupka@student.put.poznan.pl>

--- unitree_a1_legged/include/unitree_a1_legged/convert.hpp
+++ unitree_a1_legged/include/unitree_a1_legged/convert.hpp
@@ -25,90 +25,90 @@ UNITREE_LEGGED_SDK::MotorCmd rosMotorToCmd(const unitree_a1_legged_msgs::msg::Mo
 unitree_a1_legged_msgs::msg::MotorState stateToRos(const UNITREE_LEGGED_SDK::MotorState &state);
 
 
-UNITREE_LEGGED_SDK::HighCmd rosToCmd(const unitree_a1_legged_msgs::msg::HighCmd::SharedPtr msg)
-{
-    UNITREE_LEGGED_SDK::HighCmd state;
-    state.levelFlag = msg->a1.level_flag;
-    state.commVersion = msg->a1.comm_version;
-    state.robotID = msg->a1.robot_id;
-    state.SN = msg->a1.sn;
-    state.bandWidth = msg->a1.band_width;
-    state.mode = msg->mode;
-    state.forwardSpeed = msg->forward_speed;
-    state.sideSpeed = msg->side_speed;
-    state.rotateSpeed = msg->rotate_speed;
-    state.bodyHeight = msg->body_height;
-    state.footRaiseHeight = msg->foot_raise_height;
-    state.yaw = msg->yaw;
-    state.pitch = msg->pitch;
-    state.roll = msg->roll;
-    for (int i = 0; i < 4; i++)
-    {
-        state.led[i].r = msg->led[i].r;
-        state.led[i].g = msg->led[i].g;
-        state.led[i].b = msg->led[i].b;
-    }
-    for (int i = 0; i < 40; i++)
-    {
-        state.wirelessRemote[i] = msg->wireless_remote[i];
-        state.AppRemote[i] = msg->app_remote[i];
-    }
-    state.reserve = msg->reserve;
-    state.crc = msg->crc;
-    return state;
-}
+// UNITREE_LEGGED_SDK::HighCmd rosToCmd(const unitree_a1_legged_msgs::msg::HighCmd::SharedPtr msg)
+// {
+//     UNITREE_LEGGED_SDK::HighCmd state;
+//     state.levelFlag = msg->a1.level_flag;
+//     state.commVersion = msg->a1.comm_version;
+//     state.robotID = msg->a1.robot_id;
+//     state.SN = msg->a1.sn;
+//     state.bandWidth = msg->a1.band_width;
+//     state.mode = msg->mode;
+//     state.forwardSpeed = msg->forward_speed;
+//     state.sideSpeed = msg->side_speed;
+//     state.rotateSpeed = msg->rotate_speed;
+//     state.bodyHeight = msg->body_height;
+//     state.footRaiseHeight = msg->foot_raise_height;
+//     state.yaw = msg->yaw;
+//     state.pitch = msg->pitch;
+//     state.roll = msg->roll;
+//     for (int i = 0; i < 4; i++)
+//     {
+//         state.led[i].r = msg->led[i].r;
+//         state.led[i].g = msg->led[i].g;
+//         state.led[i].b = msg->led[i].b;
+//     }
+//     for (int i = 0; i < 40; i++)
+//     {
+//         state.wirelessRemote[i] = msg->wireless_remote[i];
+//         state.AppRemote[i] = msg->app_remote[i];
+//     }
+//     state.reserve = msg->reserve;
+//     state.crc = msg->crc;
+//     return state;
+// }
 
-unitree_a1_legged_msgs::msg::HighState stateToRos(const UNITREE_LEGGED_SDK::HighState &state)
-{
-    unitree_a1_legged_msgs::msg::HighState msg;
-    msg.a1.level_flag = state.levelFlag;
-    msg.a1.comm_version = state.commVersion;
-    msg.a1.robot_id = state.robotID;
-    msg.a1.sn = state.SN;
-    msg.a1.band_width = state.bandWidth;
-    msg.mode = state.mode;
-    msg.forward_speed = state.forwardSpeed;
-    msg.side_speed = state.sideSpeed;
-    msg.rotate_speed = state.rotateSpeed;
-    msg.body_height = state.bodyHeight;
-    msg.up_down_speed = state.updownSpeed;
-    msg.forward_position = state.forwardPosition;
-    msg.side_position = state.sidePosition;
-    for (int i = 4; i < 4; i++)
-    {
-        msg.foot_position_to_body[i].x = state.footPosition2Body[i].x;
-        msg.foot_position_to_body[i].y = state.footPosition2Body[i].x;
-        msg.foot_position_to_body[i].z = state.footPosition2Body[i].x;
-        msg.foot_speed_to_body[i].x = state.footSpeed2Body[i].x;
-        msg.foot_speed_to_body[i].y = state.footSpeed2Body[i].y;
-        msg.foot_speed_to_body[i].z = state.footSpeed2Body[i].z;
-        msg.foot_force[i] = state.footForce[i];
-        msg.foot_force_est[i] = state.footForceEst[i];
-    }
-    // Imu msg:
-    sensor_msgs::msg::Imu imu_msg;
-    imu_msg.header.frame_id = "imu_link";
-    imu_msg.orientation.x = state.imu.quaternion[0];
-    imu_msg.orientation.y = state.imu.quaternion[1];
-    imu_msg.orientation.z = state.imu.quaternion[2];
-    imu_msg.orientation.w = state.imu.quaternion[3];
-    imu_msg.angular_velocity.x = state.imu.gyroscope[0];
-    imu_msg.angular_velocity.y = state.imu.gyroscope[0];
-    imu_msg.angular_velocity.z = state.imu.gyroscope[0];
-    imu_msg.linear_acceleration.x = state.imu.accelerometer[0];
-    imu_msg.linear_acceleration.y = state.imu.accelerometer[1];
-    imu_msg.linear_acceleration.z = state.imu.accelerometer[2];
+// unitree_a1_legged_msgs::msg::HighState stateToRos(const UNITREE_LEGGED_SDK::HighState &state)
+// {
+//     unitree_a1_legged_msgs::msg::HighState msg;
+//     msg.a1.level_flag = state.levelFlag;
+//     msg.a1.comm_version = state.commVersion;
+//     msg.a1.robot_id = state.robotID;
+//     msg.a1.sn = state.SN;
+//     msg.a1.band_width = state.bandWidth;
+//     msg.mode = state.mode;
+//     msg.forward_speed = state.forwardSpeed;
+//     msg.side_speed = state.sideSpeed;
+//     msg.rotate_speed = state.rotateSpeed;
+//     msg.body_height = state.bodyHeight;
+//     msg.up_down_speed = state.updownSpeed;
+//     msg.forward_position = state.forwardPosition;
+//     msg.side_position = state.sidePosition;
+//     for (int i = 4; i < 4; i++)
+//     {
+//         msg.foot_position_to_body[i].x = state.footPosition2Body[i].x;
+//         msg.foot_position_to_body[i].y = state.footPosition2Body[i].x;
+//         msg.foot_position_to_body[i].z = state.footPosition2Body[i].x;
+//         msg.foot_speed_to_body[i].x = state.footSpeed2Body[i].x;
+//         msg.foot_speed_to_body[i].y = state.footSpeed2Body[i].y;
+//         msg.foot_speed_to_body[i].z = state.footSpeed2Body[i].z;
+//         msg.foot_force[i] = state.footForce[i];
+//         msg.foot_force_est[i] = state.footForceEst[i];
+//     }
+//     // Imu msg:
+//     sensor_msgs::msg::Imu imu_msg;
+//     imu_msg.header.frame_id = "imu_link";
+//     imu_msg.orientation.x = state.imu.quaternion[0];
+//     imu_msg.orientation.y = state.imu.quaternion[1];
+//     imu_msg.orientation.z = state.imu.quaternion[2];
+//     imu_msg.orientation.w = state.imu.quaternion[3];
+//     imu_msg.angular_velocity.x = state.imu.gyroscope[0];
+//     imu_msg.angular_velocity.y = state.imu.gyroscope[0];
+//     imu_msg.angular_velocity.z = state.imu.gyroscope[0];
+//     imu_msg.linear_acceleration.x = state.imu.accelerometer[0];
+//     imu_msg.linear_acceleration.y = state.imu.accelerometer[1];
+//     imu_msg.linear_acceleration.z = state.imu.accelerometer[2];
 
-    msg.imu = imu_msg;
-    msg.tick = state.tick;
-    for (int i = 0; i < 40; i++)
-    {
-        msg.wireless_remote[i] = state.wirelessRemote[i];
-    }
-    msg.reserve = state.reserve;
-    msg.crc = state.crc;
-    return msg;
-}
+//     msg.imu = imu_msg;
+//     msg.tick = state.tick;
+//     for (int i = 0; i < 40; i++)
+//     {
+//         msg.wireless_remote[i] = state.wirelessRemote[i];
+//     }
+//     msg.reserve = state.reserve;
+//     msg.crc = state.crc;
+//     return msg;
+// }
 
 UNITREE_LEGGED_SDK::LowCmd rosToCmd(const unitree_a1_legged_msgs::msg::LowCmd::SharedPtr msg)
 {
